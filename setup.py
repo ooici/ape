ec27b0fa787be7e8ea9ff592085ded042a6ebd1c
Jonathan Newbrough <jonathan.newbrough@gyregroup.com>
2012-12-19 12:24:25 -0800

    bump version

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+package_dir = {'ape': 'src/ape'}
+
+setup(
+    name='ape',
+    version='2012.12.19',
+    author='Jonathan Newbrough',
+    author_email='jonathan.newbrough@gyregroup.com',
+    url='https://github.com/ooici/ape',
+    package_dir = {'': 'src'},
+    packages=['ape', 'ape.agent', 'ape.common', 'ape.component'],
+)
