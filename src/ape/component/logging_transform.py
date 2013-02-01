a1a1bee35537ee37feb1104d5207e37205920f83
Jonathan Newbrough <jonathan.newbrough@gyregroup.com>
2013-01-31 16:11:35 -0800

    rate counter can relay value back to mgr rather than just log it

--- src/ape/component/logging_transform.py
+++ src/ape/component/logging_transform.py
@@ -1,21 +1,35 @@
 
 '''
-silly little transform to consume granules and log rates
+silly little transform to consume granules and log message rates
+
+can consume any data product to report how quickly granules were produced by that data product.
+default is to log time elapsed per 100 granules (configurable).
+if an ape agent is found running in the same container, will also send a PerformanceResult message.
 '''
 
 from ion.core.process.transform import TransformDataProcess
 from ooi.logging import log
 import threading
 import time
+from ape.common.requests import PerformanceResult
+
+APE_AGENT_NAME='testing_agent'
 
 class LoggingTransform(TransformDataProcess):
     def __init__(self,*a,**b):
         super(LoggingTransform,self).__init__(*a,**b)
+        self._label = self.CFG.get('label', 'rate_' + self._proc_name)
         self._count_lock = threading.Lock()
         self._report_lock = threading.Lock()
         self._count = 0
-        self._next = self._rate = 100 # make configurable?
+        self._next = self._rate = self.CFG.get('rate', 100)
         self._start = time.time()
+        try:
+            agent_name = self.CFG.get('agent', APE_AGENT_NAME)
+            self._agent = self.container.proc_manager.procs_by_name[agent_name]
+        except:
+            log.warn('can not send reply messages, no ape agent found in container: %s', agent_name)
+            self._agent = None
 
     def recv_packet(self, packet, stream_route, stream_id):
         log.trace("received granule: %r from stream %r", packet, stream_id)
@@ -26,7 +40,12 @@ class LoggingTransform(TransformDataProcess):
                 end = time.time()
                 elapsed = end-self._start
                 self._start = end
-                log.info('received %d messages in %d seconds', self._rate, elapsed)
+                self.report(elapsed)
+
+    def report(self, elapsed):
+        log.info('received %d messages in %d seconds', self._rate, elapsed)
+        if self._agent:
+            self._agent.report(self._label, PerformanceResult(elapsed))
 
     def increment_count(self):
         with self._count_lock:
